3bce21de158f25dd8830054b45643323dba8d0e1
Katherine-Greg <katiasimpson17@gmail.com>
2023-11-24 08:56:35 +0200

    Changed settings

--- IT_company_task_manager/urls.py
+++ IT_company_task_manager/urls.py
@@ -15,7 +15,6 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path("blog/", include("blog.urls"))
 """
 from django.contrib import admin
-from django.contrib.auth import views
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
@@ -24,8 +23,7 @@ from task_manager.views import register
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("login/", views.LoginView.as_view(template_name="registration/login.html"), name="login"),
-    path('register/', register, name="register"),
+    path("register/", register, name="register"),
     path("", include("task_manager.urls", namespace="task_manager")),
     path("accounts/", include("django.contrib.auth.urls")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
