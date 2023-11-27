238a8bd8abd6ec6fc0ead0eeed44b73ca5fc71c0
Katherine-Greg <katiasimpson17@gmail.com>
2023-11-27 03:54:44 +0200

    update settings.py

--- IT_company_task_manager/settings.py
+++ IT_company_task_manager/settings.py
@@ -42,7 +42,7 @@ SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 # SECURITY WARNING: don"t run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = ["127.0.0.1", "task-manager-ohh7.onrender.com"]
 
 INTERNAL_IPS = [
     "127.0.0.1",
